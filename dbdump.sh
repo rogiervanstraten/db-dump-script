f4b71ee5bb4f1e26f53239cf72b91d47cab9009d
Rogier van Straten <rogier@baldadig.nl>
2019-03-13 13:45:52 +0100

    Fixed tailing old backups

--- dbdump.sh
+++ dbdump.sh
@@ -45,15 +45,31 @@ tar_file=./tmp/$file_prefix$file_date.tar
 
 # run db dump
 mysqldump -u${db_user} -p${db_password} $db_name > $tmp_file
+final_file=$tmp_file
+
 tar cvf $tar_file $tmp_file
+final_file=$tar_file
 
 # check if we want to encrypt the file
 if $crypt;
   then
     # zip -r -0 -e $file_prefix$file_date.sql.zip ./tmp/$file_prefix$file_date.sql
     openssl aes-128-cbc -salt -in $tar_file -out ${tar_file}.aes -k $crypt_key
+    final_file=${tar_file}.aes
     if [ -f "$tar_file" ]; then rm -f $tar_file; fi
 fi
 
 # Remove tmp file
 if [ -f "$tmp_file" ]; then rm -f $tmp_file; fi
+
+# Copy to target path
+mv $final_file $target_dir
+
+
+if $clearout;
+  then
+    echo 'TASK -> Remove old backups'
+    echo 'Tail ' $clearout_tail
+    cd $target_dir
+    ls -t | tail -n +$clearout_tail | xargs rm -rf --
+fi
